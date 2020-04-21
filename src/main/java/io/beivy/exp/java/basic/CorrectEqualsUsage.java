76806c071df083d838c54a64e1f4096920a73e94
Yongwei Dou <yongwei.dou@gmail.com>
2020-04-22 06:35:18 +0900

    added the float equal objects.

--- src/main/java/io/beivy/exp/java/basic/CorrectEqualsUsage.java
+++ src/main/java/io/beivy/exp/java/basic/CorrectEqualsUsage.java
@@ -1,6 +1,12 @@
 package io.beivy.exp.java.basic;
 
+import java.math.BigDecimal;
+
 public class CorrectEqualsUsage {
+
+    public boolean isEquals(float data, float anotherData) {
+        return data == anotherData;
+    }
 
     public boolean isFoo(String str) {
         return str.equals("foo");
@@ -9,5 +15,8 @@ public class CorrectEqualsUsage {
     public static void main(String[] args) {
         CorrectEqualsUsage usage = new CorrectEqualsUsage();
         System.out.println(usage.isFoo("abc"));
+
+        System.out.println(usage.isEquals((1.0f - 0.9f), (0.9f - 0.8f)));
+
     }
 }
